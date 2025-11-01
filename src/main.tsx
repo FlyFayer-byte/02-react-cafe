f83340233e4b5ebdc42f740633eaf1ac9a73e439
FlyFayer-byte <krupa.mykhailo@icloud.com>
2025-11-01 23:19:05 +0100

    renamed:    src/App.module.css -> src/components/App/App.module.css 	renamed:    src/App.tsx -> src/components/App/App.tsx 	modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+// import { StrictMode } from 'react';
+import React from 'react';
+// import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 // import './index.css'
-import './App.module.css';
-import App from './App.tsx';
+// import './App.module.css';
+import App from './components/App/App.tsx';
 // Нормалізація стилів
 import 'modern-normalize';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
