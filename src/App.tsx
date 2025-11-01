3f06ddb3782ba668a3270a3abc00889ff8e69a64
FlyFayer-byte <krupa.mykhailo@icloud.com>
2025-11-01 16:21:11 +0100

    modified:   src/App.tsx 	deleted:    src/index.css 	modified:   src/main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import css from "./App.module.css"
+import appCss from "./App.module.css"
+import cafeInfoCss from "./components/CafeInfo/CafeInfo.module.css"
 
 export default function App() {
   return (
-    <div className={css.app}></div>
+    <div className={appCss.app}>
+      <div className={cafeInfoCss.container}>
+        <h1 className={cafeInfoCss.title}>Sip Happens Café</h1>
+        <p className={cafeInfoCss.description}>
+          Please rate our service by selecting one of the options below.
+        </p>
+      </div>
+    </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
+import './App.module.css'
 import App from './App.tsx'
 // Нормалізація стилів
 import "modern-normalize";
